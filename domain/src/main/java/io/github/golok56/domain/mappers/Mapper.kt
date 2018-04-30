5626a3bb89081bec79fc655965b356f8499aaba2
Satria Adi Putra <satadii11@gmail.com>
2018-04-30 16:14:16 +0700

    change Observable -> Single

--- domain/src/main/java/io/github/golok56/domain/mappers/Mapper.kt
+++ domain/src/main/java/io/github/golok56/domain/mappers/Mapper.kt
@@ -1,11 +1,11 @@
 package io.github.golok56.domain.mappers
 
-import io.reactivex.Observable
+import io.reactivex.Single
 
 abstract class Mapper<in E, T> {
     abstract fun mapFrom(from: E): T
 
-    fun observable(from: E): Observable<T> = Observable.fromCallable { mapFrom(from) }
-    fun observable(from: List<E>): Observable<List<T>> =
-            Observable.fromCallable { from.map { mapFrom(it) } }
+    fun observable(from: E): Single<T> = Single.fromCallable { mapFrom(from) }
+    fun observable(from: List<E>): Single<List<T>> =
+            Single.fromCallable { from.map { mapFrom(it) } }
 }
