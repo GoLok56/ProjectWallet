1d3d11269f5e2fb1dbedb472917c83becf4a2bcf
Satria Adi Putra <satadii11@gmail.com>
2018-05-01 08:04:06 +0700

    add TransactionDao

---
+++ data/src/main/java/io/github/golok56/data/db/TransactionDao.kt
@@ -0,0 +1,20 @@
+package io.github.golok56.data.db
+
+import android.arch.persistence.room.*
+import io.github.golok56.data.entities.TransactionData
+import io.reactivex.Single
+
+@Dao
+interface TransactionDao {
+    @Query("SELECT * FROM transactions")
+    fun getAll(): Single<List<TransactionData>>
+
+    @Insert
+    fun insert(transaction: TransactionData): Int
+
+    @Delete
+    fun delete(transaction: TransactionData): Int
+
+    @Update
+    fun update(transaction: TransactionData): Int
+}
