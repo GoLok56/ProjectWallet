be9dcf59b6cfb2802366ae2cc717427d898a8d41
Satria Adi Putra <satadii11@gmail.com>
2018-05-01 08:25:59 +0700

    add TransactionData

---
+++ data/src/main/java/io/github/golok56/data/entities/TransactionData.kt
@@ -0,0 +1,18 @@
+package io.github.golok56.data.entities
+
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.ForeignKey
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "transactions",
+        foreignKeys = [ForeignKey(entity = CategoryData::class, parentColumns = ["id"],
+                childColumns = ["categoryId"])])
+class TransactionData(
+        @PrimaryKey var id: Int = -1,
+        var amount: Int,
+        var moneyLeft: Int,
+        var date: String,
+        var description: String,
+        var type: String,
+        var categoryId: Int
+)
